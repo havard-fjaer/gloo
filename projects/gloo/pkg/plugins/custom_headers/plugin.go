0f6010656739c4de86ba45dadf72afa36cd00e60
Håvard Fjær <haavard.fjaer@fremtind.no>
2021-10-25 15:03:24 +0200

    Adding custom_headers plugin

---
+++ projects/gloo/pkg/plugins/custom_headers/plugin.go
@@ -0,0 +1,155 @@
+package custom_headers
+
+import (
+	envoy_config_core_v3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
+	envoy_config_route_v3 "github.com/envoyproxy/go-control-plane/envoy/config/route/v3"
+	"github.com/solo-io/gloo/pkg/utils/api_conversion"
+	v1 "github.com/solo-io/gloo/projects/gloo/pkg/api/v1"
+	"github.com/solo-io/gloo/projects/gloo/pkg/api/v1/options/custom_headers"
+	"github.com/solo-io/gloo/projects/gloo/pkg/plugins"
+	"github.com/solo-io/solo-kit/pkg/errors"
+)
+
+var (
+	MissingHeaderValueError = errors.Errorf("header section of header value option cannot be nil")
+)
+
+// Puts Header Manipulation config on Routes, VirtualHosts, and Weighted Clusters
+type Plugin struct{}
+
+var _ plugins.RoutePlugin = NewPlugin()
+var _ plugins.VirtualHostPlugin = NewPlugin()
+var _ plugins.WeightedDestinationPlugin = NewPlugin()
+
+func NewPlugin() *Plugin {
+	return &Plugin{}
+}
+
+func (p *Plugin) Init(_ plugins.InitParams) error {
+	return nil
+}
+
+func (p *Plugin) ProcessWeightedDestination(
+	params plugins.RouteParams,
+	in *v1.WeightedDestination,
+	out *envoy_config_route_v3.WeightedCluster_ClusterWeight,
+) error {
+	headerManipulation := in.GetOptions().GetHeaderManipulation()
+	if headerManipulation == nil {
+		return nil
+	}
+
+	envoyHeader, err := convertHeaderConfig(headerManipulation, getSecretsFromSnapshot(params.Snapshot))
+	if err != nil {
+		return err
+	}
+
+	out.RequestHeadersToAdd = envoyHeader.RequestHeadersToAdd
+	out.RequestHeadersToRemove = envoyHeader.RequestHeadersToRemove
+	out.ResponseHeadersToAdd = envoyHeader.ResponseHeadersToAdd
+	out.ResponseHeadersToRemove = envoyHeader.ResponseHeadersToRemove
+
+	return nil
+}
+
+func (p *Plugin) ProcessVirtualHost(
+	params plugins.VirtualHostParams,
+	in *v1.VirtualHost,
+	out *envoy_config_route_v3.VirtualHost,
+) error {
+	headerManipulation := in.GetOptions().GetHeaderManipulation()
+
+	if headerManipulation == nil {
+		return nil
+	}
+
+	envoyHeader, err := convertHeaderConfig(headerManipulation, getSecretsFromSnapshot(params.Snapshot))
+	if err != nil {
+		return err
+	}
+
+	out.RequestHeadersToAdd = envoyHeader.RequestHeadersToAdd
+	out.RequestHeadersToRemove = envoyHeader.RequestHeadersToRemove
+	out.ResponseHeadersToAdd = envoyHeader.ResponseHeadersToAdd
+	out.ResponseHeadersToRemove = envoyHeader.ResponseHeadersToRemove
+
+	return nil
+}
+
+func (p *Plugin) ProcessRoute(params plugins.RouteParams, in *v1.Route, out *envoy_config_route_v3.Route) error {
+	headerManipulation := in.GetOptions().GetHeaderManipulation()
+
+	if headerManipulation == nil {
+		return nil
+	}
+
+	envoyHeader, err := convertHeaderConfig(headerManipulation, getSecretsFromSnapshot(params.Snapshot))
+	if err != nil {
+		return err
+	}
+
+	out.RequestHeadersToAdd = envoyHeader.RequestHeadersToAdd
+	out.RequestHeadersToRemove = envoyHeader.RequestHeadersToRemove
+	out.ResponseHeadersToAdd = envoyHeader.ResponseHeadersToAdd
+	out.ResponseHeadersToRemove = envoyHeader.ResponseHeadersToRemove
+
+	return nil
+}
+
+type envoyHeaderManipulation struct {
+	RequestHeadersToAdd     []*envoy_config_core_v3.HeaderValueOption
+	RequestHeadersToRemove  []string
+	ResponseHeadersToAdd    []*envoy_config_core_v3.HeaderValueOption
+	ResponseHeadersToRemove []string
+}
+
+func getSecretsFromSnapshot(snapshot *v1.ApiSnapshot) *v1.SecretList {
+	var secrets *v1.SecretList
+	if snapshot == nil {
+		secrets = &v1.SecretList{}
+	} else {
+		secrets = &snapshot.Secrets
+	}
+	return secrets
+}
+
+func convertHeaderConfig(in *custom_headers.HeaderManipulation, secrets *v1.SecretList) (*envoyHeaderManipulation, error) {
+	// request headers can either be made from a normal key/value pair, or.
+	// they can be constructed from a supplied secret. To accomplish this, we use
+	// a utility function that was originally created to accomplish this for health check headers.
+	requestAdd, err := api_conversion.ToEnvoyHeaderValueOptionList(in.GetRequestHeadersToAdd(), secrets)
+	if err != nil {
+		return nil, err
+	}
+	// response headers have no reason to include secrets.
+	responseAdd, err := convertResponseHeaderValueOption(in.GetResponseHeadersToAdd())
+	if err != nil {
+		return nil, err
+	}
+
+	return &envoyHeaderManipulation{
+		RequestHeadersToAdd:     requestAdd,
+		RequestHeadersToRemove:  in.GetRequestHeadersToRemove(),
+		ResponseHeadersToAdd:    responseAdd,
+		ResponseHeadersToRemove: in.GetResponseHeadersToRemove(),
+	}, nil
+}
+
+func convertResponseHeaderValueOption(
+	in []*custom_headers.HeaderValueOption,
+) ([]*envoy_config_core_v3.HeaderValueOption, error) {
+	var out []*envoy_config_core_v3.HeaderValueOption
+	for _, h := range in {
+		if h.GetHeader() == nil {
+			return nil, MissingHeaderValueError
+		}
+		out = append(out, &envoy_config_core_v3.HeaderValueOption{
+			Header: &envoy_config_core_v3.HeaderValue{
+				Key:   h.GetHeader().GetKey(),
+				Value: h.GetHeader().GetValue(),
+			},
+			Append: h.GetAppend(),
+		})
+	}
+	return out, nil
+}
---
+++ projects/gloo/pkg/plugins/custom_headers/plugin_test.go
@@ -0,0 +1,158 @@
+package custom_headers
+
+import (
+	envoy_config_core_v3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
+	envoy_config_route_v3 "github.com/envoyproxy/go-control-plane/envoy/config/route/v3"
+	"github.com/golang/protobuf/ptypes/wrappers"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	v1 "github.com/solo-io/gloo/projects/gloo/pkg/api/v1"
+	"github.com/solo-io/gloo/projects/gloo/pkg/api/v1/options/custom_headers"
+	"github.com/solo-io/gloo/projects/gloo/pkg/plugins"
+	envoycore_sk "github.com/solo-io/solo-kit/pkg/api/external/envoy/api/v2/core"
+	coreV1 "github.com/solo-io/solo-kit/pkg/api/v1/resources/core"
+)
+
+var _ = Describe("Plugin", func() {
+	p := NewPlugin()
+	It("errors if the request header is nil", func() {
+		out := &envoy_config_route_v3.Route{}
+		err := p.ProcessRoute(plugins.RouteParams{}, &v1.Route{
+			Options: &v1.RouteOptions{
+				HeaderManipulation: testBrokenConfigNoRequestHeader,
+			},
+		}, out)
+
+		Expect(err).To(HaveOccurred())
+		Expect(err.Error()).To(Equal("Unexpected header option type <nil>"))
+	})
+	It("errors if the response header is nil", func() {
+		out := &envoy_config_route_v3.Route{}
+		err := p.ProcessRoute(plugins.RouteParams{}, &v1.Route{
+			Options: &v1.RouteOptions{
+				HeaderManipulation: testBrokenConfigNoResponseHeader,
+			},
+		}, out)
+		Expect(err).To(HaveOccurred())
+		Expect(err).To(Equal(MissingHeaderValueError))
+	})
+	It("converts the header manipulation config for weighted destinations", func() {
+		out := &envoy_config_route_v3.WeightedCluster_ClusterWeight{}
+		err := p.ProcessWeightedDestination(plugins.RouteParams{}, &v1.WeightedDestination{
+			Options: &v1.WeightedDestinationOptions{
+				HeaderManipulation: testHeaderManip,
+			},
+		}, out)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(out.RequestHeadersToAdd).To(Equal(expectedHeaders.RequestHeadersToAdd))
+		Expect(out.RequestHeadersToRemove).To(Equal(expectedHeaders.RequestHeadersToRemove))
+		Expect(out.ResponseHeadersToAdd).To(Equal(expectedHeaders.ResponseHeadersToAdd))
+		Expect(out.ResponseHeadersToRemove).To(Equal(expectedHeaders.ResponseHeadersToRemove))
+	})
+	It("converts the header manipulation config for virtual hosts", func() {
+		out := &envoy_config_route_v3.VirtualHost{}
+		err := p.ProcessVirtualHost(plugins.VirtualHostParams{}, &v1.VirtualHost{
+			Options: &v1.VirtualHostOptions{
+				HeaderManipulation: testHeaderManip,
+			},
+		}, out)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(out.RequestHeadersToAdd).To(Equal(expectedHeaders.RequestHeadersToAdd))
+		Expect(out.RequestHeadersToRemove).To(Equal(expectedHeaders.RequestHeadersToRemove))
+		Expect(out.ResponseHeadersToAdd).To(Equal(expectedHeaders.ResponseHeadersToAdd))
+		Expect(out.ResponseHeadersToRemove).To(Equal(expectedHeaders.ResponseHeadersToRemove))
+	})
+	It("converts the header manipulation config for routes", func() {
+		out := &envoy_config_route_v3.Route{}
+		err := p.ProcessRoute(plugins.RouteParams{}, &v1.Route{
+			Options: &v1.RouteOptions{
+				HeaderManipulation: testHeaderManip,
+			},
+		}, out)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(out.RequestHeadersToAdd).To(Equal(expectedHeaders.RequestHeadersToAdd))
+		Expect(out.RequestHeadersToRemove).To(Equal(expectedHeaders.RequestHeadersToRemove))
+		Expect(out.ResponseHeadersToAdd).To(Equal(expectedHeaders.ResponseHeadersToAdd))
+		Expect(out.ResponseHeadersToRemove).To(Equal(expectedHeaders.ResponseHeadersToRemove))
+	})
+	It("Can add secrets to headers", func() {
+		paramsWithSecret := plugins.VirtualHostParams{
+			Params: plugins.Params{
+				Snapshot: &v1.ApiSnapshot{
+					Secrets: v1.SecretList{
+						{
+							Kind: &v1.Secret_Header{
+								Header: &v1.HeaderSecret{
+									Headers: map[string]string{
+										"Authorization": "basic dXNlcjpwYXNzd29yZA==",
+									},
+								},
+							},
+							Metadata: &coreV1.Metadata{
+								Name:      "foo",
+								Namespace: "bar",
+							},
+						},
+					},
+				},
+			},
+		}
+
+		out := &envoy_config_route_v3.VirtualHost{}
+		err := p.ProcessVirtualHost(paramsWithSecret, &v1.VirtualHost{
+			Options: &v1.VirtualHostOptions{
+				HeaderManipulation: testHeaderManipWithSecrets,
+			},
+		}, out)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(out.RequestHeadersToAdd).To(Equal(expectedHeadersWithSecrets.RequestHeadersToAdd))
+		Expect(out.RequestHeadersToRemove).To(Equal(expectedHeadersWithSecrets.RequestHeadersToRemove))
+		Expect(out.ResponseHeadersToAdd).To(Equal(expectedHeadersWithSecrets.ResponseHeadersToAdd))
+		Expect(out.ResponseHeadersToRemove).To(Equal(expectedHeadersWithSecrets.ResponseHeadersToRemove))
+	})
+})
+
+var testBrokenConfigNoRequestHeader = &custom_headers.HeaderManipulation{
+	RequestHeadersToAdd:     []*envoycore_sk.HeaderValueOption{{HeaderOption: nil, Append: &wrappers.BoolValue{Value: true}}},
+	RequestHeadersToRemove:  []string{"a"},
+	ResponseHeadersToAdd:    []*custom_headers.HeaderValueOption{{Header: &custom_headers.HeaderValue{Key: "foo", Value: "bar"}, Append: &wrappers.BoolValue{Value: true}}},
+	ResponseHeadersToRemove: []string{"b"},
+}
+
+var testBrokenConfigNoResponseHeader = &custom_headers.HeaderManipulation{
+	RequestHeadersToAdd: []*envoycore_sk.HeaderValueOption{{HeaderOption: &envoycore_sk.HeaderValueOption_Header{Header: &envoycore_sk.HeaderValue{Key: "foo", Value: "bar"}},
+		Append: &wrappers.BoolValue{Value: true}}},
+	RequestHeadersToRemove:  []string{"a"},
+	ResponseHeadersToAdd:    []*custom_headers.HeaderValueOption{{Header: nil, Append: &wrappers.BoolValue{Value: true}}},
+	ResponseHeadersToRemove: []string{"b"},
+}
+
+var testHeaderManip = &custom_headers.HeaderManipulation{
+	RequestHeadersToAdd: []*envoycore_sk.HeaderValueOption{{HeaderOption: &envoycore_sk.HeaderValueOption_Header{Header: &envoycore_sk.HeaderValue{Key: "foo", Value: "bar"}},
+		Append: &wrappers.BoolValue{Value: true}}},
+	RequestHeadersToRemove:  []string{"a"},
+	ResponseHeadersToAdd:    []*custom_headers.HeaderValueOption{{Header: &custom_headers.HeaderValue{Key: "foo", Value: "bar"}, Append: &wrappers.BoolValue{Value: true}}},
+	ResponseHeadersToRemove: []string{"b"},
+}
+
+var expectedHeaders = envoyHeaderManipulation{
+	RequestHeadersToAdd:     []*envoy_config_core_v3.HeaderValueOption{{Header: &envoy_config_core_v3.HeaderValue{Key: "foo", Value: "bar"}, Append: &wrappers.BoolValue{Value: true}}},
+	RequestHeadersToRemove:  []string{"a"},
+	ResponseHeadersToAdd:    []*envoy_config_core_v3.HeaderValueOption{{Header: &envoy_config_core_v3.HeaderValue{Key: "foo", Value: "bar"}, Append: &wrappers.BoolValue{Value: true}}},
+	ResponseHeadersToRemove: []string{"b"},
+}
+
+var testHeaderManipWithSecrets = &custom_headers.HeaderManipulation{
+	RequestHeadersToAdd: []*envoycore_sk.HeaderValueOption{{HeaderOption: &envoycore_sk.HeaderValueOption_HeaderSecretRef{HeaderSecretRef: &coreV1.ResourceRef{Name: "foo", Namespace: "bar"}},
+		Append: &wrappers.BoolValue{Value: true}}},
+	RequestHeadersToRemove:  []string{"a"},
+	ResponseHeadersToAdd:    []*custom_headers.HeaderValueOption{{Header: &custom_headers.HeaderValue{Key: "foo", Value: "bar"}, Append: &wrappers.BoolValue{Value: true}}},
+	ResponseHeadersToRemove: []string{"b"},
+}
+
+var expectedHeadersWithSecrets = envoyHeaderManipulation{
+	RequestHeadersToAdd:     []*envoy_config_core_v3.HeaderValueOption{{Header: &envoy_config_core_v3.HeaderValue{Key: "Authorization", Value: "basic dXNlcjpwYXNzd29yZA=="}, Append: &wrappers.BoolValue{Value: true}}},
+	RequestHeadersToRemove:  []string{"a"},
+	ResponseHeadersToAdd:    []*envoy_config_core_v3.HeaderValueOption{{Header: &envoy_config_core_v3.HeaderValue{Key: "foo", Value: "bar"}, Append: &wrappers.BoolValue{Value: true}}},
+	ResponseHeadersToRemove: []string{"b"},
+}
